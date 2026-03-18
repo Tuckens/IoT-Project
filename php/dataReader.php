02299bcf811cd0cf397f9945cfaa5723039ec6d6
16Flavio <230736@umons.ac.be>
2026-03-18 16:19:21 +0100

    added the super secret password for the data between esp and rasp

--- php/dataReader.php
+++ php/dataReader.php
@@ -1,19 +1,34 @@
 <?php
-$temperature = isset($_POST['t']) ? $_POST['t'] : 'Inconnue';
-$compteur = isset($_POST['count']) ? $_POST['count'] : 'Inconnu';
+$correct_password = "SuperSecret";
+$flask_url = "http://127.0.0.1:5000/store_data";
 
-error_log("Mesure reçue -> Temp: " . $temperature . "°C | Count: " . $compteur);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    $temp = isset($_POST['t']) ? $_POST['t'] : null;
+    $count = isset($_POST['count']) ? $_POST['count'] : null;
+    $pwd = isset($_POST['pwd']) ? $_POST['pwd'] : null;
 
-$donnees = array(
-    'date' => date('Y-m-d H:i:s'),
-    'temperature' => $temperature,
-    'compteur' => $compteur
-);
-
-$ligne_a_sauvegarder = json_encode($donnees) . PHP_EOL;
-
-$chemin_fichier = '/home/boss/data.txt';
-file_put_contents($chemin_fichier, $ligne_a_sauvegarder, FILE_APPEND);
-
-echo "Données bien enregistrées sur la Raspberry !";
+    if ($pwd === $correct_password) {
+        
+        $ch = curl_init($flask_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+            'temperature' => $temp,
+            'count' => $count
+        ]));
+        
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        echo "Success: Data accepted and forwarded to database.";
+    } else {
+        // HTTP 401 Unauthorized
+        http_response_code(401); 
+        echo "Error: Invalid password.";
+    }
+} else {
+    // HTTP 405 Method Not Allowed
+    http_response_code(405);
+    echo "Error: Only POST requests are allowed.";
+}
 ?>
