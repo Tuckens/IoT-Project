d0df0120ced46632655b502db7f5fb06ec7afd3a
gmroue01 <95682994+gmroue01@users.noreply.github.com>
2026-03-19 17:43:15 +0100

    First version of website displaying something basic (login,register and acces to data interface)

--- flask/flaskr/app.py
+++ flask/flaskr/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, redirect, url_for, render_template
 from auth import auth_bp
+from blog import blog_bp
 
 app = Flask(__name__)
 
-app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(auth_bp, url_prefix='/api/auth')
+app.register_blueprint(blog_bp, url_prefix='/blog')
 
 
 @app.route('/')
 def index():
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('auth.api_login'))
 
 
 if __name__ == '__main__':
