1993a14ad51124833e9b0806bf6b9e1b0958993c
RedZunder <alt47r@gmail.com>
2026-03-19 11:12:55 +0100

    Fixed DNS resolve

--- esp-code/creds.h
+++ esp-code/creds.h
@@ -3,6 +3,7 @@
 #define _CREDS_H_
 const char* ssid = "Antenne";
 const char* password = "bulldograt25";
+const char* target_ip="10.92.161.212";
 
 
 
--- esp-code/esp-code.ino
+++ esp-code/esp-code.ino
@@ -6,16 +6,17 @@
 #define DEBUG_MODE    //Comment out to supress debug info in Serial
 //#define NOWIFI        //Comment out when using RaspberryPI
 
-#define LED 2         //Status LED ->built-in
-#define ERR 14        //Error LED -> D5
-#define PIR 13        //HC-SR501 -> D7
+#define LED 2           //Status LED ->built-in
+#define ERR 14          //Error LED -> D5
+#define PIR 13          //HC-SR501 -> D7
 
+String pi_hostname = "bpem.local";  
+    
 
 
 Adafruit_BMP280 bmp;
 
-const char* target_ip="10.92.161.212:8000";
-String serverName;
+IPAddress serverIP;
 
 
 
@@ -70,24 +71,40 @@ void setup()
 
 
 unsigned int count=0;   //mssg ID
-char buff[128]="";
 
 WiFiClient wifi;
 HTTPClient http;
 
 void loop()
 {
+
 #ifdef DEBUG_MODE
   Serial.print("Temperature: ");
   Serial.print(bmp.readTemperature());
   Serial.print(" Motion: ");
   Serial.println(digitalRead(PIR));
 #endif
+  /*---------------------------mDNS RESOLUTION---------------------------*/
+  
+  
+  while(!WiFi.hostByName(pi_hostname.c_str(),serverIP)||serverIP.toString() == "0.0.0.0")
+  {
+    Serial.println("DNS failed, retrying...");
+    delay(1000);
+  }
+
+  #ifdef DEBUG_MODE
+  Serial.print("Resolved Raspberry Pi IP: ");
+  Serial.println(serverIP.toString());
+  #endif
+/*-----------------------END OF mDNS RESOLUTION------------------------*/
 
 /*---------------------------HTTP CONNECTION---------------------------*/
 #ifndef NOWIFI
-  snprintf(buff,sizeof(buff),"http://%s",target_ip);
-  String path2server=String(buff);
+  char buff[128]="";
+
+  memcpy(buff,"http://",strlen("http://"));
+  strcat(buff,serverIP.toString().c_str());
   
   #ifdef DEBUG_MODE
   Serial.print("Connecting to ");
