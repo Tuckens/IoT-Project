0e48f6d658ffd745d3712a49f7316b38b41b133d
16Flavio <230736@umons.ac.be>
2026-03-19 20:57:51 +0100

    added the display of data in the web page

--- flask/flaskr/config.py
+++ flask/flaskr/config.py
@@ -1,16 +1,19 @@
 import os
 
 class Config:
-    # Set to True to generate random sensor data 
+    # Set to True to generate random sensor data
     MOCK_SENSORS = False
-    
-    # Set to True to use the computer's local webcam instead of an external stream
-    MOCK_VIDEO = True
-    
+
+    # Set to True to use the computer's local webcam instead of the Pi camera stream
+    MOCK_VIDEO = False
+
+    # Rolling window (seconds) returned by GET /api/blog/sensor_data
+    SENSOR_WINDOW_SECONDS = 60
+
     # Database Configuration
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///iot_demo.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
     # Secret key for sessions
     SECRET_KEY = os.environ.get('SECRET_KEY') or "dev-key-for-iot-dashboard"
 
